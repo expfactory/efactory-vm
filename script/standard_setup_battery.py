6ba45ccc892784304654b0084bdfb07d7e176595
vsoch <vsochat@stanford.edu>
2015-10-16 09:31:40 -0700

    adding battery standard set up script, and (updated but not complete) Vagrantfile

---
+++ script/standard_setup_battery.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+# Standard battery setup script
+# This script will generate a battery for all valid experiments, taking as input
+# a battery folder, an experiment folder, and an output directory.
